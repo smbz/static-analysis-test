c7d3ca9767b1623f654e359523dffe173e5b4f32
Andrew Ryrie <smb314159@gmail.com>
2024-09-03 15:49:54 +0100

    Add project files.

---
+++ static-analysis-test/TestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace static_analysis_test
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Index(string id)
+        {
+            // Should get error about possible out-of-bounds string access
+            if (id[0] == 'A')
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult XsrfVulnerability(string x)
+        {
+            // Textbook XSRF/CSRF vulnerability
+            var authCookie = Request.Cookies["auth"];
+            if (authCookie != "some_auth_value")
+            {
+                return Forbid();
+            }
+            ProtectedAction(x);
+            return Ok();
+        }
+
+        private void ProtectedAction(string arg)
+        {
+            // Something with a side-effect
+            System.IO.File.WriteAllBytes("C:/test.xyz", Encoding.UTF8.GetBytes(arg));
+        }
+
+    }
+}
